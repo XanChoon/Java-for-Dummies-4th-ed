afcf30fb3f145e3bc80008022371bcbb5dad30a5
XanChoon <smithchoon@gmail.com>
2018-10-11 23:33:09 +0800

    Typing Execrise

---
+++ PayBarry.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+
+class PayBarry{
+    public static void main (String args[]){
+        double checkAmount = 123.123;
+        out.print ("Pay to the order of ");
+        out.print ("Dr. barry Burd ");
+        out.print ("$");
+        out.println(checkAmount);
+    }
+
+}
