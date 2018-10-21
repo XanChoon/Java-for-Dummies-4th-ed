f3d35f6f55bf232dee819570aeb133cf7557fa1e
Potato Leong <smithchoon@gmail.com>
2018-10-21 21:25:58 +0800

    version2

--- SimpleBankTransaction.java
+++ SimpleBankTransaction.java
@@ -1,24 +1,34 @@
-class Balance{
-    double bankbalance;
-    double bankbankin;
-    double withdraw;
+class SavingAccount{
     
-    double latestbalance(){
-        return bankbalance + bankbankin - withdraw;
-    }
+    private double balance;
+    
+    public SavingAccount(){
+		balance = 0;	
+	}
+    
+    public SavingAccount(double initialBalance){
+		balance = initialBalance;
+	}
+
+	public void bankbankin(double amount){
+		balance = balance + amount;
+	}
+    
+    public void withdraw(double amount){
+		balance = balance - amount;
+	}
+    
+    public double totalbalance(){
+		return balance;
+	}
 }
 
 class SimpleBankTransaction{
     public static void main (String [] args){
-        Balance mybalance = new Balance();
-        double totalbalance;
-        
-        mybalance.bankbalance = 1000;
-        mybalance.bankbankin = 500;
-        mybalance.withdraw = 250;
 
-        totalbalance = mybalance.latestbalance();
-        System.out.println ("Sir/Madam your latestbalance is RM" + totalbalance);
+        SavingAccount mybalance = new SavingAccount(1000);
+        mybalance.bankbankin(500);
+        mybalance.withdraw(123);
+        System.out.println ("Sir/Madam your latestbalance is RM" + mybalance.totalbalance());
     }
-     
 }
