3c577f19aeef91b40c8864c8f9830728699560ed
Potato Leong <smithchoon@gmail.com>
2018-11-06 13:32:38 +0800

    learning

---
+++ BlockSample.java
@@ -0,0 +1,12 @@
+class BlockSample{
+    public static void main(String[]args){
+        int a,b;
+        a = 20;
+        
+        for (b = 0; b < 10; b++){
+            System.out.println("This A :" + a);
+            System.out.println("This B :" + b);
+            a = a - 2;
+        }
+    }
+}
