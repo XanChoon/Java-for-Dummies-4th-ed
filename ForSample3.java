41312443e4c26919137cc0b92f49bfee26323864
Potato Leong <smithchoon@gmail.com>
2018-11-06 11:11:26 +0800

    Learning

---
+++ ForSample3.java
@@ -0,0 +1,7 @@
+class ForSample3{
+    public static void main (String [] args){
+        for (int a = 10; a >= 0; a-=2 ){
+            System.out.println("Your number is decrement by 2: " + a);
+        }
+    }
+}
