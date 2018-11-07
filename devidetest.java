2c506d98584be5193d5dd745a4f488e117357e87
Potato Leong <smithchoon@gmail.com>
2018-11-07 08:07:50 +0800

    learning

---
+++ devidetest.java
@@ -0,0 +1,8 @@
+class devidetest{
+    public static void main(String[]args){
+        int a = 50;
+        int b = 3;
+        int c = a/b;
+    System.out.println("This is answer " + c);
+    }
+}
---
+++ printtest.java
@@ -0,0 +1,6 @@
+class printtest{
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        System.out.println("Alexandra Abramov");
+    }
+}
