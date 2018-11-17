483b11171bbbcc069bc111e6249c295e86e3f42e
Potato Leong <smithchoon@gmail.com>
2018-11-17 14:21:09 +0800

    learning

--- BasicMath.java
+++ BasicMath.java
@@ -2,14 +2,28 @@ class BasicMath{
     public static void main (String [] args){
         int a = 1 + 1;
         int b = a * 3;
-        int c = b / 4;
+        int c = b / b;
         int d = c - a;
-        int e = -d;
+        int e = c;
         
-        System.out.println ("a = " + a);
-        System.out.println ("b = " + b);
-        System.out.println ("c = " + c);
-        System.out.println ("d = " + d);
-        System.out.println ("e = " + e);
+        System.out.println("This is a integer math results");
+        System.out.println("a " + a);
+        System.out.println("b " + b);
+        System.out.println("c " + c);
+        System.out.println("d " + d);
+        System.out.println("e " + e);
+        
+        double da = 1 + 1;
+        double db = 2 * 3;
+        double dc = db / 4;
+        double dd = dc - da;
+        double de = da - dc;
+        
+        System.out.println("\nThis is a double math results");
+        System.out.println("da " + da);
+        System.out.println("db " + db);
+        System.out.println("dc " + dc);
+        System.out.println("dd " + dd);
+        System.out.println("de " + de);
     }
 }
---
+++ Modulus.java
@@ -0,0 +1,10 @@
+class Modulus{
+    public static void main (String [] args){
+        int x = 4;
+        double y = 42.25;
+        
+        System.out.println("This x " + x%10);
+        System.out.println("This y " + y%10);
+        
+    }
+}
